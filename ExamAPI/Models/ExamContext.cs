5d9a6442a6c40713b138fa4f35108bdddbff63f3
Gabriel <G.Bischof.inf22@stud.bbbaden.ch>
2024-02-21 10:38:52 +0100

    API und Datenbank mit docker compose als docker image zum laufen gebracht. API und Datenbank funktionieren reibungslos auf einem Docker Image.

--- ExamAPI/Models/ExamContext.cs
+++ ExamAPI/Models/ExamContext.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using Microsoft.EntityFrameworkCore.Infrastructure; 
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Linq.Expressions;
 namespace ExamAPI.Models
 {
     public class ExamContext : DbContext
     {
         public ExamContext (DbContextOptions<ExamContext> options) : base (options) 
-        { 
-        
+        {
+            try { 
+            var databaseCreator = Database.GetService<IRelationalDatabaseCreator>() as RelationalDatabaseCreator;
+
+            if (databaseCreator != null)
+            {
+                if (!databaseCreator.CanConnect()) databaseCreator.Create();
+                if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
+            }
+
+            }
+            catch (Exception ex) 
+            {
+            Console.WriteLine (ex.Message);
+            }
         }
         
         public DbSet<Exam> exams { get; set; }
--- ExamAPI/Program.cs
+++ ExamAPI/Program.cs
@@ -5,12 +5,27 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ExamContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("ExamCS")));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+/* Database Context Injection */
+
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST") ;
+var dbName =Environment.GetEnvironmentVariable("DB_NAME") ;
+var dbPassword =Environment.GetEnvironmentVariable("DB_SA_PASSWORD") ;
+
+// Setzen Sie den ServerCertificateValidationCallback
+System.Net.ServicePointManager.ServerCertificateValidationCallback =
+    (sender, certificate, chain, sslPolicyErrors) => { return true; };
+
+var ConnectionString = $"Server={dbHost};Initial Catalog={dbName};User Id=sa;Password={dbPassword};Encrypt=False;";
+/* ======================== */
+
+builder.Services.AddDbContext<ExamContext>(options => options.UseSqlServer(ConnectionString));
 
 var app = builder.Build();
 
